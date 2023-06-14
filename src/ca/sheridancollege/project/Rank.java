941bf64f31fb8716f001e479ed91c56f2dbc4f17
anike <anike@10.16.9.51>
2023-06-14 12:29:20 -0400

    Changed the code of CardTrick

---
+++ src/ca/sheridancollege/project/Rank.java
@@ -0,0 +1,9 @@
+package ca.sheridancollege.project;
+
+/**
+ * An enumeration representing the rank of a card in blackjack.
+ */
+public enum Rank {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
+
